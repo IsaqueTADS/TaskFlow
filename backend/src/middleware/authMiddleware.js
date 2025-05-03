93c6e2bc43bc266f116ec742708bf8b77dbfe52a
IsaqueTADS <isaqueofc18@gmail.com>
2025-05-03 17:29:59 -0300

    fix: add missing line req.userId = decoded.userId in authMiddleware

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -9,7 +9,8 @@ export const authenticate = (req, res, next) => {
   }
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    next()
+    req.userId = decoded.userId;
+    next();
   } catch (error) {
     res.status(401).json({ error: "Token inválido" });
   }
