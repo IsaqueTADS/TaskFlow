191ca95b4ce40caf1676bb2ea969f0d8b295c38f
IsaqueTADS <isaqueofc18@gmail.com>
2025-05-03 15:31:12 -0300

    feat: add authMiddleware for route protection

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
+export const authenticate = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ error: "Token não fornecido" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    res.status(401).json({ error: "Token inválido" });
+  }
+};
