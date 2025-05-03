97f0bc05db1dc8094f230cd2556de4379fa02612
IsaqueTADS <isaqueofc18@gmail.com>
2025-05-03 17:13:12 -0300

    feat: add try-catch for error handling in task search and check for empty tasks in user ID

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -1,10 +1,24 @@
 import prisma from "../utils/prisma.js";
 
 export const getTask = async (req, res) => {
-  const task = await Prisma.task.findMany({
-    where: { userId: req.userId },
-  });
-  res.json(task);
+  try {
+    const task = await prisma.task.findMany({
+      where: { userId: req.userId },
+    });
+
+    if (task.length === 0) {
+      return res.status(200).json({
+        message: "Nenhuma tarefa encontrada para este usuário.",
+        tasks: [],
+      });
+    }
+
+    res.status(200).json(task);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Erro interno no servidor ao buscar tarefas" });
+  }
 };
 
 export const createTask = async (req, res) => {
